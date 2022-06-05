a837ae28fdfe1569fa7b263cad5c013b2dda48b1
小野 和也 <prvogo@gmail.com>
2022-06-05 11:57:01 +0900

    <su-menu>コンポーネント使用時、メニューが右にはみ出す問題の修正

--- src/mixins/menuable/index.js
+++ src/mixins/menuable/index.js
@@ -42,22 +42,26 @@ export default {
       const dCon = this.dimensions.content
 
       const gutter = this.$soloui.layout.gutter
-      const gutterWidth = getAppAbsolutedInnerElement() ? this.rectangle(getAppAbsolutedInnerElement()).width : this.rectangle(this.$root.$el).width - (gutter * 2)
+      const innerWidth = getAppAbsolutedInnerElement() ? this.rectangle(getAppAbsolutedInnerElement()).width : this.rectangle(this.$root.$el).width - (gutter * 2)
+
+      let left = 0
 
       if( this.offsetX ){
         if( this.left ){
           const contentL = dAct.left - dCon.width
 
-          return gutter < contentL ? `${contentL}px` : `0`
+          left = gutter < contentL ? contentL : 0
         } else {
           const contentL = dAct.right
           const contentR = contentL + dCon.width
 
-          return gutterWidth > contentR ? `${contentL}px` : gutterWidth > dCon.width ? `${contentL - (contentR - gutterWidth)}px` : '0'
+          left = innerWidth > contentR ? contentL : innerWidth > dCon.width ? contentL - (contentR - innerWidth) : 0
         }
       } else {
-        return `${dAct.left}px`;
+        left = dAct.left
       }
+
+      return `${innerWidth < (left + dCon.width) ? innerWidth - dCon.width : left}px`
     },
     calcContentTop(){
       const dAct = this.dimensions.activator
